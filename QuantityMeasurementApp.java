4414d8237cec9100d00433a6b58d9b1abc19ef98
Anjali <ar7361@srmist.edu.in>
2026-04-22 10:11:23 +0530

    Refactor to generic quantity class for UC3

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,23 +1,14 @@
 public class QuantityMeasurementApp {
 
-    public static boolean compareFeet(double value1, double value2) {
-        Feet feet1 = new Feet(value1);
-        Feet feet2 = new Feet(value2);
-        return feet1.equals(feet2);
-    }
-
-    public static boolean compareInches(double value1, double value2) {
-        Inches inch1 = new Inches(value1);
-        Inches inch2 = new Inches(value2);
-        return inch1.equals(inch2);
-    }
-
     public static void main(String[] args) {
 
-        System.out.println(compareFeet(1.0, 1.0));     // true
-        System.out.println(compareFeet(1.0, 2.0));     // false
+        Quantity oneFoot = new Quantity(1.0, Quantity.Unit.FEET);
+        Quantity twelveInches = new Quantity(12.0, Quantity.Unit.INCHES);
+        Quantity twoFeet = new Quantity(2.0, Quantity.Unit.FEET);
+        Quantity twentyFourInches = new Quantity(24.0, Quantity.Unit.INCHES);
 
-        System.out.println(compareInches(1.0, 1.0));   // true
-        System.out.println(compareInches(1.0, 2.0));   // false
+        System.out.println(oneFoot.equals(twelveInches));      // true
+        System.out.println(twoFeet.equals(twentyFourInches));  // true
+        System.out.println(oneFoot.equals(twoFeet));           // false
     }
 }
