62944c881ea81520a79cc835d96e5c27094475ab
Anjali <ar7361@srmist.edu.in>
2026-04-22 10:37:29 +0530

    Implement UC6 addition of two length units

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,41 +1,21 @@
 public class QuantityMeasurementApp {
 
-    public static void demonstrateLengthConversion(double value,
-                                                   Quantity.Unit from,
-                                                   Quantity.Unit to) {
-
-        double result = Quantity.convert(value, from, to);
-
-        System.out.println(value + " " + from + " = " + result + " " + to);
-    }
-
-    public static void demonstrateLengthConversion(Quantity quantity,
-                                                   Quantity.Unit to) {
-
-        Quantity converted = quantity.convertTo(to);
-
-        System.out.println(quantity + " = " + converted);
-    }
-
     public static void main(String[] args) {
 
-        demonstrateLengthConversion(1.0,
-                Quantity.Unit.FEET,
-                Quantity.Unit.INCHES);
+        Quantity q1 = new Quantity(1.0, Quantity.Unit.FEET);
+        Quantity q2 = new Quantity(12.0, Quantity.Unit.INCHES);
+        System.out.println(q1.add(q2)); // 2.0 FEET
 
-        demonstrateLengthConversion(3.0,
-                Quantity.Unit.YARDS,
-                Quantity.Unit.FEET);
+        Quantity q3 = new Quantity(12.0, Quantity.Unit.INCHES);
+        Quantity q4 = new Quantity(1.0, Quantity.Unit.FEET);
+        System.out.println(q3.add(q4)); // 24.0 INCHES
 
-        demonstrateLengthConversion(36.0,
-                Quantity.Unit.INCHES,
-                Quantity.Unit.YARDS);
+        Quantity q5 = new Quantity(1.0, Quantity.Unit.YARDS);
+        Quantity q6 = new Quantity(3.0, Quantity.Unit.FEET);
+        System.out.println(q5.add(q6)); // 2.0 YARDS
 
-        demonstrateLengthConversion(1.0,
-                Quantity.Unit.CENTIMETERS,
-                Quantity.Unit.INCHES);
-
-        Quantity length = new Quantity(2.0, Quantity.Unit.YARDS);
-        demonstrateLengthConversion(length, Quantity.Unit.INCHES);
+        Quantity q7 = new Quantity(2.54, Quantity.Unit.CENTIMETERS);
+        Quantity q8 = new Quantity(1.0, Quantity.Unit.INCHES);
+        System.out.println(q7.add(q8)); // ~5.08 CENTIMETERS
     }
 }
